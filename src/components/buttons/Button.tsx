6c487e0dc02c3874cfce85b1ed1b546a2c29e4ac
lucas <lucas.lvn97439@gmail.com>
2025-02-05 11:03:55 +0500

    update button

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -4,11 +4,15 @@ interface ButtonProps {
   text: string;
   onClick: () => void;
   class?: string;
+  size?: "small" | "medium" | "large";
 }
 
 export function Button(props: ButtonProps) {
   return (
-    <button class={"default-button " + props.class} onClick={props.onClick}>
+    <button
+      class={(props.size ? props.size + "-button  " : "") + "default-button "}
+      onClick={props.onClick}
+    >
       {props.text}
     </button>
   );

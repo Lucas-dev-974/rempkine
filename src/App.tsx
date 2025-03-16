bda77fd2279b21e43ef9b7bfdd1167b79ac824ce
lucas <lucas.lvn97439@gmail.com>
2025-03-16 12:58:17 +0500

    setup floating menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { FloatingMenu } from "./components/floating menu/FloatingMenu";
 import { Notification } from "./components/notification/Notification";
 import { FetcherService } from "./services/fetch.service";
-import { Navbar } from "./components/navbar/Navbar";
-import { RouteManager } from "./router/Router";
 import { createEffect, on, onMount } from "solid-js";
-import { loggedIn } from "./const.data";
+import { Navbar } from "./components/navbar/Navbar";
 import storeService from "./utils/store.service";
+import { RouteManager } from "./router/Router";
+import { loggedIn } from "./const.data";
 
 export function App() {
   onMount(() => FetcherService.setHost(import.meta.env.VITE_HOST));

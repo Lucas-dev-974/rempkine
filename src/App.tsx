e69a6db8c31c3fff94aced10c5a68ae25a5c9266
lucas <lucas.lvn97439@gmail.com>
2025-03-21 23:03:35 +0500

    add event on login signal

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { Notification } from "./components/notification/Notification";
 import { FetcherService } from "./services/fetch.service";
 import { Navbar } from "./components/navbar/Navbar";
 import { RouteManager } from "./router/Router";
-import { onMount } from "solid-js";
+import { createEffect, on, onMount } from "solid-js";
+import { loggedIn } from "./const.data";
+import storeService from "./utils/store.service";
 
 export function App() {
   onMount(() => FetcherService.setHost(import.meta.env.VITE_HOST));
+  createEffect(on(loggedIn, () => (storeService.proxy.isLogin = loggedIn())));
 
   return (
     <main>

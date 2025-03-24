b56127163770a9d4392645409264fca743632812
lucas <lucas.lvn97439@gmail.com>
2025-03-24 10:28:50 +0500

    add "form" theme on LabeledInput

--- src/components/inputs/LabeledInput.tsx
+++ src/components/inputs/LabeledInput.tsx
@@ -1,26 +1,36 @@
+import { createSignal, onMount } from "solid-js";
 import "./LabeledInput.css";
 
 interface LabeledInputProps {
   id: string;
   label: string;
-  type: "text" | "mail" | "date" | "number";
+  type: "text" | "mail" | "date" | "number" | "password";
   placeholder?: string;
 
+  style?: "form";
   value?: string;
+  required?: boolean;
 
   onInput?: (e: Event & { target: any & { value: string } }) => void;
 }
 
 export function LabeledInput(props: LabeledInputProps) {
+  const [classname, setClassname] = createSignal("labeled-input");
+
+  onMount(() =>
+    setClassname((prev) => (!!props.style ? `${prev}-${props.style}` : prev))
+  );
   return (
-    <div class="labeled-input">
+    <div class={classname()}>
       <label for={props.id}>{props.label}</label>
       <input
         type={props.type}
         id={props.id}
+        name={props.id}
         placeholder={props.placeholder ? props.placeholder : ""}
         onInput={(e) => props.onInput && props.onInput(e)}
         value={props.value || ""}
+        required={props.required}
       />
     </div>
   );

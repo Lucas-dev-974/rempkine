83520aa476f57010a39eda926148b2fb4fe7f19e
lucas lvn <lucas.lvn97439@gmail.com>
2025-03-30 12:43:43 +0200

    refactor & add css file

--- src/components/floating-menu/FloatingMenu.tsx
+++ src/components/floating-menu/FloatingMenu.tsx
@@ -1,14 +1,15 @@
-import { FilesOutlineIcon } from "../../icons/FilesOutlineIcon";
+import { FloatingMenuItems } from "./bottom-menu-dialog/FloatingMenuItems";
+import { BottomMenuDialog } from "./bottom-menu-dialog/BottomMenuDialog";
+
+import "./FloatingMenu.css";
 
 export function FloatingMenu() {
   return (
-    <div class="fixed bottom-5 w-full flex justify-center overflow-visible z-0">
-      <div class="bg-slate-300 shadow-2xl rounded-xl px-5 py-4 ">
-        <div class="min-w-5 flex flex-col items-center gap-2">
-          <FilesOutlineIcon />
-          <p class="text-xs">Fichiers</p>
-        </div>
+    <div class="floating-bottom-menu-container  ">
+      <div class="floating-bottom-menu">
+        <FloatingMenuItems />
       </div>
+      <BottomMenuDialog />
     </div>
   );
 }

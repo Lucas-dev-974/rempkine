6c560e1ea2a0d44db9bfc7991417390fa1466c51
lucas lvn <lucas.lvn97439@gmail.com>
2025-04-02 12:00:14 +0200

    hidde bottom menu is user not logged in

--- src/components/floating-menu/FloatingMenu.tsx
+++ src/components/floating-menu/FloatingMenu.tsx
@@ -1,15 +1,19 @@
 import { FloatingMenuItems } from "./bottom-menu-dialog/FloatingMenuItems";
 import { BottomMenuDialog } from "./bottom-menu-dialog/BottomMenuDialog";
+import { loggedIn } from "../../const.data";
+import { Show } from "solid-js";
 
 import "./FloatingMenu.css";
 
 export function FloatingMenu() {
   return (
-    <div class="floating-bottom-menu-container  ">
-      <div class="floating-bottom-menu">
-        <FloatingMenuItems />
+    <Show when={loggedIn()}>
+      <div class="floating-bottom-menu-container  ">
+        <div class="floating-bottom-menu">
+          <FloatingMenuItems />
+        </div>
+        <BottomMenuDialog />
       </div>
-      <BottomMenuDialog />
-    </div>
+    </Show>
   );
 }

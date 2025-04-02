d3eec4a1da434e96929ce7770c12f0e21ab70518
lucas lvn <lucas.lvn97439@gmail.com>
2025-04-02 09:55:46 +0200

    to review

--- src/components/dialog/DialogWrapper.tsx
+++ src/components/dialog/DialogWrapper.tsx
@@ -2,27 +2,28 @@ import { createSignal, JSX, Show } from "solid-js";
 import { Button } from "../buttons/Button";
 
 import "./DialogWrapper.css";
-type DialogWrapperProps = {
+
+interface DialogWrapperProps {
   children: JSX.Element;
   btnText: string;
   title: string;
-};
+}
+
+// ! TODO review this code, if we have multiple use of DialogWrapper this externalised openDialog gonna open them all
+const [isOpen, setIsOpen] = createSignal(false);
+export const openDialogTool = () => setIsOpen(true);
+const closeDialogTool = () => setIsOpen(false);
 
 export function DialogWrapper(props: DialogWrapperProps) {
-  const [isOpen, setIsOpen] = createSignal(false);
-
-  const openDialog = () => setIsOpen(true);
-  const closeDialog = () => setIsOpen(false);
-
   return (
     <>
-      <Button text={props.btnText} onClick={openDialog} />
+      <Button text={props.btnText} onClick={openDialogTool} />
       <Show when={isOpen()} fallback={null}>
-        <div class="dialog-overlay" onClick={closeDialog}>
+        <div class="dialog-overlay" onClick={closeDialogTool}>
           <div class="dialog" onClick={(e) => e.stopPropagation()}>
             <div class="dialog-header">
               <h3>{props.title}</h3>
-              <button class="close-button" onClick={closeDialog}>
+              <button class="close-button" onClick={closeDialogTool}>
                 &times;
               </button>
             </div>

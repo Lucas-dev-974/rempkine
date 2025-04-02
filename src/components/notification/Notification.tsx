0e22e3ef58b70e26cb81adbc4c6a4d6be7097e26
lucas lvn <lucas.lvn97439@gmail.com>
2025-04-02 13:33:30 +0200

    update Z index of notification

--- src/components/notification/Notification.tsx
+++ src/components/notification/Notification.tsx
@@ -1,11 +1,10 @@
-import { For } from "solid-js";
 import { notifications } from "../../utils/notification.service";
-import { CloseIcon } from "../../icons/CloseIcon";
 import { NotifcationItem } from "./NotifcationItem";
+import { For } from "solid-js";
 
 export function Notification() {
   return (
-    <div class="absolute flex flex-col p-5 gap-2 right-0">
+    <div class="absolute flex flex-col p-5 gap-2 right-0 z-50">
       <For each={notifications()}>
         {(notif) => <NotifcationItem {...notif} />}
       </For>

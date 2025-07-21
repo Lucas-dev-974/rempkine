d7332c5b2671acb991c1805a6abd5a1350f706f8
lucas lvn <lucas.lvn97439@gmail.com>
2025-07-21 22:50:10 +0200

    use Title1 component

--- src/views/Contracts.tsx
+++ src/views/Contracts.tsx
@@ -1,5 +1,7 @@
 import { ContractList } from "../components/contract/contract-list/ContractList";
 import { Button } from "../components/buttons/Button";
+import { Title1 } from "../components/titles/Title1";
+import { Motion } from "@motionone/solid";
 
 export function Contracts() {
   function createContract() {
@@ -7,13 +9,22 @@ export function Contracts() {
   }
 
   return (
-    <div>
-      <div class="flex  justify-between items-center">
-        <h1 class="text-xl ">Mes contrats</h1>
-        <Button onClick={createContract} text="Proposer un contrat" />
-      </div>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="pt-10 pb-16 sm:pt-16 sm:pb-20 lg:pt-20 lg:pb-28 text-center lg:text-left">
+        <Motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+        >
 
-      <ContractList />
+          <div class="flex  justify-between items-center">
+            <Title1 text="Mes contrats" color="primary" />
+            <Button onClick={createContract} text="Proposer un contrat" />
+          </div>
+
+          <ContractList />
+        </Motion.div>
+      </div>
     </div>
   );
 }

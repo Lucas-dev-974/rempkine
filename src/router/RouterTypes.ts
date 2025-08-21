7fe4d8c2ed10792d2fded5066fc2d6f25ccb2305
lucas lvn <lucas.lvn97439@gmail.com>
2025-08-21 20:05:29 +0200

    try git action fix build

--- src/router/RouterTypes.ts
+++ src/router/RouterTypes.ts
@@ -6,5 +6,5 @@ export enum PagesEnum {
     register = "/register",
 }
 
-export const [onPage, setPage] = createSignal<PagesEnum>(PagesEnum.home);
 export const publicPages: PagesEnum[] = [PagesEnum.home, PagesEnum.login, PagesEnum.register];
+export const [onPage, setPage] = createSignal<PagesEnum>(PagesEnum.home);

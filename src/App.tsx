2492b483b6eb20a4719af170a29614ef90ecf049
lucas lvn <lucas.lvn97439@gmail.com>
2025-08-21 19:52:48 +0200

    try git action fix build

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { FloatingMenu } from "./components/floating-menu/FloatingMenu";
 import { createEffect, on, onMount } from "solid-js";
 import { Navbar } from "./components/navbar/Navbar";
 import storeService from "./utils/store.service";
-import { loggedIn } from "./const.data";
 import { Router } from "./router/router";
+import { loggedIn } from "./const.data";
 
 export function App() {
   onMount(() => FetcherService.setHost(import.meta.env.VITE_HOST));

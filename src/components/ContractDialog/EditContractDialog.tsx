7b64c800aae4b5c5e2b42d325f77e643990db898
Lucas lvn <lucas.lvn.dev@protonmail.com>
2026-01-26 11:20:32 +0100

    Refactor EditContractDialog to dynamically set dialog title: Introduce a title function that adjusts the title based on the contract state, improving clarity for users when signing or updating contracts.

--- src/components/ContractDialog/EditContractDialog.tsx
+++ src/components/ContractDialog/EditContractDialog.tsx
@@ -4,14 +4,25 @@ import { DialogWrapper } from "../dialog/DialogWrapper";
 import { ContratInformationsDropdowns } from "./DropdownContratInformations/ContratInformationsDropdowns";
 import { PDFTool } from "../../utils/PDFTool";
 import { loadContract } from "../../const.data";
+import { signeBack } from "../../pages/PageWrapper";
 
 
 export function EditContractDialog() {
+  function title() {
+    if (signeBack()) {
+      return "Signer un contrat";
+    } else if (loadContract()) {
+      return "Mettre à jour  un contrat";
+    } else {
+      return "Edité un nouveau contrat";
+    }
+  }
   return (
     <DialogWrapper
       btnText="Créer un contrat"
-      title={loadContract() ? "Mettre à jour  un contrat" : "Edité un nouveau contrat"}
+      title={title()}
       dialogClass="w-[90vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] xl:w-[40vw] bg-slate-200 rounded-lg"
+
     >
       <div class="p-3 max-h-[70vh]">
         <Show when={currentPDFTool() instanceof PDFTool}>

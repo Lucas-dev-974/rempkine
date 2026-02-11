8ee1c2edcb1a79dbf40535c6551abd51036f616c
Lucas lvn <lucas.lvn.dev@protonmail.com>
2026-02-11 07:00:31 +0100

    Update RegisterInformationsInLocal component to use toDateString for birthday value, ensuring proper date format handling.

--- src/components/RegisterInformationsInLocal/RegisterInformationsInLocal.tsx
+++ src/components/RegisterInformationsInLocal/RegisterInformationsInLocal.tsx
@@ -100,7 +100,7 @@ export function RegisterInformationsInLocal(props: { isInNavbar?: boolean }) {
                     id="birthday"
                     label="Date de naissance"
                     type="date"
-                    value={birthday().toString()}
+                    value={birthday().toDateString() ?? "z"}
                     onInput={(e) => setBirthday(new Date(e.target.value))}
                 />
                 <LabeledInput

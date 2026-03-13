2c3746e3ce136fa44e435d07f5bbc39478142ade
Lucas lvn <lucas.lvn.dev@protonmail.com>
2026-03-13 12:43:24 +0100

    Fix z-index of FloatingMenu to 200

--- src/components/FloattingMenu/floating-menu/FloatingMenu.tsx
+++ src/components/FloattingMenu/floating-menu/FloatingMenu.tsx
@@ -7,7 +7,7 @@ import { Show } from "solid-js";
 export function FloatingMenu() {
   return (
     <Show when={onPage() != PagesEnum.login && onPage() != PagesEnum.register}>
-      <div class="fixed bottom-0 w-full flex justify-center overflow-visible z-0">
+      <div class="fixed bottom-0 w-full flex justify-center overflow-visible z-[200]">
         <div class="w-full text-white shadow-2xl px-5 py-2 flex gap-10  justify-center"
           style={{ "background": "linear-gradient(360deg,rgba(9, 151, 115, 1) 0%, rgba(67, 182, 146, 1) 100%)" }}>
           <FloatingMenuItems />

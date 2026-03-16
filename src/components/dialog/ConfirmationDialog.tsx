fe8f1bff9db0db7622d3575429709bc69995f975
Lucas lvn <lucas.lvn.dev@protonmail.com>
2026-03-16 09:57:10 +0100

    refactor padding and implement the component in a Portal

--- src/components/dialog/ConfirmationDialog.tsx
+++ src/components/dialog/ConfirmationDialog.tsx
@@ -1,3 +1,5 @@
+import { Portal } from "solid-js/web";
+import { Button } from "../buttons/Button";
 import { OutlinedButton } from "../buttons/OulinedButton";
 import { DialogWrapper } from "./DialogWrapper";
 
@@ -13,36 +15,39 @@ interface ConfirmationDialogProps {
 
 export function ConfirmationDialog(props: ConfirmationDialogProps) {
     return (
-        <DialogWrapper
-            name="confirmationDialog"
-            btnText=""
-            title={props.title}
-            isInNavbar={false}
-            isOpen={props.isOpen}
-            onClose={props.onCancel}
-            hideTriggerButton
-        >
-            <div class="mb-6">
-                <p class="text-gray-700 font-[Nunito] m-0">
-                    {props.message}
-                </p>
-            </div>
+        <Portal>
+            <DialogWrapper
+                name="confirmationDialog"
+                btnText=""
+                title={props.title}
+                isInNavbar={false}
+                isOpen={props.isOpen}
+                onClose={props.onCancel}
+                hideTriggerButton
+            >
+                <div class="mb-6">
+                    <p class="text-gray-700 font-[Nunito] m-0 text-center">
+                        {props.message}
+                    </p>
+                </div>
 
-            <div class="flex gap-3 justify-end">
-                <OutlinedButton
-                    text={props.cancelText || "Annuler"}
-                    onClick={props.onCancel}
-                    size="medium"
-                    class="flex-1 sm:flex-none"
-                />
-                <button
-                    class="font-[Nunito] text-base px-4 py-2 rounded-lg cursor-pointer text-white duration-700 hover:shadow-lg border-none flex-1 sm:flex-none bg-red-600 hover:bg-red-700"
-                    onClick={props.onConfirm}
-                >
-                    {props.confirmText || "Confirmer"}
-                </button>
-            </div>
-        </DialogWrapper>
+                <div class="flex gap-3 justify-end ">
+                    <OutlinedButton
+                        text={props.cancelText || "Annuler"}
+                        onClick={props.onCancel}
+                        size="medium"
+                        class="flex-1 sm:flex-none"
+                    />
+                    <Button
+                        text={props.confirmText || "Confirmer"}
+                        onClick={props.onConfirm}
+                        size="medium"
+                        class="flex-1 sm:flex-none"
+                        isDanger={true}
+                    />
+                </div>
+            </DialogWrapper>
+        </Portal>
     );
 }
 
